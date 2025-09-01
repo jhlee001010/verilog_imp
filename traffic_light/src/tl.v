e54411aff206b0708ed08c9f043e1057f6874b95
이제희 <jhlee001010@gmail.com>
2025-09-01 11:11:42 +0900

    0901

---
+++ traffic_light/src/tl.v
@@ -0,0 +1,4 @@
+module tl_controller (
+
+
+);
---
+++ vending_machine/src/vm_ver1.v
@@ -0,0 +1,15 @@
+module vending_machine_controller(
+    input clk;
+    input reset;
+    
+    input Ba;
+    input Bb;
+    input Bc;
+    
+    input Sc;
+    input Sp;
+
+
+
+);
+
